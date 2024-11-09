25d9a46794c8f2743a2af9112712a577e0f892a3
xlDownxl <niclas-joswig@gmx.de>
2024-11-09 08:20:14 +0200

    Initial Commit

---
+++ junction5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  junction5
+//
+//  Created by Joswig, Niclas on 9.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ junction5/junction5App.swift
@@ -0,0 +1,17 @@
+//
+//  junction5App.swift
+//  junction5
+//
+//  Created by Joswig, Niclas on 9.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct junction5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
